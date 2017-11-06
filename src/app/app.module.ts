678a3ba01da7ae62c9baaf3bfabfa0b01d9bf3d8
dqmdz <daniel.quinterospinto@gmail.com>
2017-11-05 21:17:42 -0300

    2nd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+
+import { GithubAuthInterceptor } from './githubauth.interceptor';
 
 @NgModule({
   declarations: [
--- src/app/githubauth.interceptor.ts
+++ src/app/githubauth.interceptor.ts
@@ -5,6 +5,9 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class GithubAuthInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(req);
+        const authReq = req.clone({
+            headers: req.headers.set('Authorization', 'token 32578d1adfacf8003a5c3e39694722d91cae0038')
+        });
+        return next.handle(authReq);
     }
 }
